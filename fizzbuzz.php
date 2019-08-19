4f38f604c1972412cb89073830d25585ef1e5bd5
Lin Trieu <lin@Lins-MacBook-Pro.local>
2019-08-19 14:29:08 +0100

    Minor refactors and formatting

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,38 +1,35 @@
 <?php
 
-class Fizzbuzzing {
+class Fizzbuzzing
+{
+    function speak()
+    {
+        echo "Hello, PHP world! \n";
+        for ($i = 0; $i <= 100; $i++) {
+            echo $this->fizzbuzz($i) . "\n";
+        }
+    }
 
-  function speak() {
-      echo "Hello, PHP world! \n";
-      for ($i=0; $i<=100; $i++) {
-        echo $this->fizzbuzz($i) . "\n";
-      }
-  }
-
-  function fizzbuzz($number) {
-      if ($number == 0) {
-          echo $number;
-      } elseif ($number % 3 == 0 && $number % 5 == 0) {
-          echo "Fizzbuzz";
-      } elseif ($number % 3 == 0) {
-          echo "Fizz";
-      } elseif ($number % 5 == 0) {
-          echo "Buzz";
-      } else {
-          echo $number;
-      }
-  }
-  // work on this function. How do I get feedback in PHP to invoke another method and print list of numbers 1-100??
-//  function numbers() {
-//      for ($i = 0; $i < 100; $i++) {
-//        fizzbuzz(i);
-//      }
-//  }
+    function fizzbuzz($number)
+    {
+        if ($number == 0) {
+            echo $number;
+        } elseif ($number % 3 == 0 && $number % 5 == 0) {
+            echo "Fizzbuzz";
+        } elseif ($number % 3 == 0) {
+            echo "Fizz";
+        } elseif ($number % 5 == 0) {
+            echo "Buzz";
+        } else {
+            echo $number;
+        }
+    }
 }
 
-# Methods can be used dynamically
+# Methods can be invoked in PHP by multiple approaches.
+# E.g. Dynamically - invoke a method on an instance of an object
 $obj = new Fizzbuzzing();
-$obj->speak(5);
+$obj->speak();
 
-//#Methods can be used statically
+//# E.g. Statically - invoke a method on the class blueprint itself, if the method does not require parameters or object context
 //Fizzbuzzing::speak();
